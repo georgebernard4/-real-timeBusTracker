ddc1d5a1707fd6b999096345b8450d6df27c29bc
georgebernard4 <georgebernard4@gmail.com>
2022-06-27 00:54:50 -0400

    inital commit

---
+++ pasteYourMapboxKeyInHere.js
@@ -0,0 +1,14 @@
+//replace your key here to keep it from being tracked
+//Go to Mapbox.com
+//signup
+//credit card will be required
+//copy the API Key here
+//As of now, June 2022, 
+//a public key will be free up to 50,000 map loads for Web
+//and 200,000 Static Tile API requests
+//You are unlikely to pass these limits using this program personally
+//So keep your API key private by only putting it here
+  let MapboxKeyXX = 'copy key here'; 
+//let MapboxKeyXX = pk.lKjJmqfPCVls30qrlv6v3xmIWKxT8RKbThGZMZ4oOcsNigQU69W2f8v6d4YBH8X3mEQw8k3I-YJX6UCr67hjuhuZMa29'
+//Your key should look siimilar to commented-out key
+//Note: commented out key randomly generated with the help of https://www.hjkeen.net/htoys/generate.htm
