10960c0fcbf7661167ab997ffe65d3ca14475d8b
georgebernard4 <georgebernard4@gmail.com>
2022-06-28 02:02:59 -0400

    navbar polished

--- pasteYourMapboxKeyInHere.js
+++ pasteYourMapboxKeyInHere.js
@@ -8,7 +8,28 @@
 //and 200,000 Static Tile API requests
 //You are unlikely to pass these limits using this program personally
 //So keep your API key private by only putting it here
+//you can refresh your API key here: https://account.mapbox.com/access-tokens/
+//remove their access to your credit card here: 
+
   let MapboxKeyXX = 'copy key here'; 
+  let MapboxkeyXX = 'pk.eyJ1IjoiZ2VvcmdlYmVybmFyZDQiLCJhIjoiY2w0d2IwMWNmMDB3dDNjbnI1c3Zmd2I0diJ9.E2NLSQpSBGP3YBLhmudqOg'
 //let MapboxKeyXX = pk.lKjJmqfPCVls30qrlv6v3xmIWKxT8RKbThGZMZ4oOcsNigQU69W2f8v6d4YBH8X3mEQw8k3I-YJX6UCr67hjuhuZMa29'
 //Your key should look siimilar to commented-out key
 //Note: commented out key randomly generated with the help of https://www.hjkeen.net/htoys/generate.htm
+
+
+//use mapbox without key
+//https://stackoverflow.com/questions/57251504/use-mapbox-gl-js-without-access-token
+
+//to delete mapbox.com's access to your credit card go to 
+//https://account.mapbox.com/settings/billing
+//scroll down to end of page
+//and click remove credit card
+
+//deleting the key - I recommend you instead refresh your key (see above) and don't copy the new key, just close your browser
+//key id is needed and its not clear where you get that see:
+//https://docs.mapbox.com/api/accounts/tokens/#delete-a-token
+//https://github.com/mapbox/mapbox-sdk-js/blob/main/docs/services.md#deletetoken
+
+
+//https://www.youtube.com/c/MapboxOfficial/videos
